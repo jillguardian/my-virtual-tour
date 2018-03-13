560b35b09813cfc823b60ff9453ba7a9d8012faa
Angela Guardian <jillguardian@gmail.com>
2018-03-13 13:41:45 +0800

    Remove produced ZIP file after build + update email credentials + fix Jackson's no-default-constructor complaints

--- server/src/main/java/ph/edu/tsu/tour/web/common/dto/ChangePasswordPayload.java
+++ server/src/main/java/ph/edu/tsu/tour/web/common/dto/ChangePasswordPayload.java
@@ -1,14 +1,20 @@
 package ph.edu.tsu.tour.web.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Data
-@lombok.Builder(builderClassName = "Builder", toBuilder = true)
 public final class ChangePasswordPayload implements Serializable {
 
     private static final long serialVersionUID = -3525318407975978104L;
@@ -22,5 +28,11 @@ public final class ChangePasswordPayload implements Serializable {
     @Size(min = 1, message = "{password.blank.message}")
     @JsonProperty("password")
     private String newPassword;
+
+    @JsonCreator
+    public static ChangePasswordPayload create(@JsonProperty("token") String newPasswordToken,
+                                               @JsonProperty("password") String newPassword) {
+        return new ChangePasswordPayload(newPasswordToken, newPassword);
+    }
 
 }
--- server/src/main/java/ph/edu/tsu/tour/web/common/dto/UserPayload.java
+++ server/src/main/java/ph/edu/tsu/tour/web/common/dto/UserPayload.java
@@ -1,5 +1,9 @@
 package ph.edu.tsu.tour.web.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -8,6 +12,7 @@ import java.io.Serializable;
 
 @Data
 @lombok.Builder(builderClassName = "Builder", toBuilder = true)
+@JsonDeserialize(builder = UserPayload.Builder.class)
 public final class UserPayload implements Serializable {
 
     private static final long serialVersionUID = -5428176942386069327L;
@@ -23,5 +28,10 @@ public final class UserPayload implements Serializable {
     @NotNull(message = "{email.blank.message}")
     @Size(min = 1, message = "{email.blank.message}")
     private String email;
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class Builder {
+
+    }
 
 }
